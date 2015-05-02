f43449f91a6b26e65858a8191485f86cdcb5816d
dkim89 <dkim89@bu.edu>
2015-05-02 15:12:56 -0400

    added missing file clock.v

---
+++ Final_Project_FPGAs/Verilog_Code/Bluetooth_Control/clock.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:14:16 04/29/2015 
+// Design Name: 
+// Module Name:    clock 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module clock(
+    output reg clk_9600=0, 
+					output clk_2,clk_4,clk_8,
+					input clk
+    );
+	 reg [30:0]count=0;
+	 reg [15:0]count2=0;
+	 reg [30:0]count3=0;
+	 
+	 always@(posedge clk)
+	 count<=count+1'b1;
+	 
+	 always@(posedge clk)
+	 begin
+	 count2<=count2+1'b1;
+	 if (count2==5207)//test x=1->1/4		2(x+1)=10416+x/3   //x=5207+1/3
+	 begin
+	 clk_9600<=~clk_9600;
+	 count2<=0;
+	 end
+	 end
+	 
+	 
+	 assign clk_2=count[0];
+	 assign clk_4=count[1];
+	 assign clk_8=count[2];
+
+
+endmodule
